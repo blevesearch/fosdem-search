e0facb91a6dcc4d651c91873c27ca0a0009ea04c
Marty Schoch <marty.schoch@gmail.com>
2015-09-16 17:13:54 -0400

    changes for new mapping

--- mapping.go
+++ mapping.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"github.com/blevesearch/bleve"
+	"github.com/blevesearch/bleve/analysis/analyzers/keyword_analyzer"
+	"github.com/blevesearch/bleve/analysis/language/en"
 )
 
 func buildMapping() *bleve.IndexMapping {
 	enFieldMapping := bleve.NewTextFieldMapping()
-	enFieldMapping.Analyzer = "en"
+	enFieldMapping.Analyzer = en.AnalyzerName
 
 	eventMapping := bleve.NewDocumentMapping()
 	eventMapping.AddFieldMappingsAt("summary", enFieldMapping)
 	eventMapping.AddFieldMappingsAt("description", enFieldMapping)
 
 	kwFieldMapping := bleve.NewTextFieldMapping()
-	kwFieldMapping.Analyzer = "keyword"
+	kwFieldMapping.Analyzer = keyword_analyzer.Name
 
 	eventMapping.AddFieldMappingsAt("url", kwFieldMapping)
 	eventMapping.AddFieldMappingsAt("category", kwFieldMapping)
 
 	mapping := bleve.NewIndexMapping()
 	mapping.DefaultMapping = eventMapping
-	mapping.DefaultAnalyzer = "en"
+	mapping.DefaultAnalyzer = en.AnalyzerName
 
 	return mapping
 }
